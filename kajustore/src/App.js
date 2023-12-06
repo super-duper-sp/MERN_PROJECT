49ca3ee1ab6abab828f202056a79e6517fd281c1
shubham patidar <shubham15362@gmail.com>
2023-12-06 12:50:13 -0800

    frontend correction

--- kajustore/src/App.js
+++ kajustore/src/App.js
@@ -1,1 +1,22 @@
-kajustore/src/App.js
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './index.css';
+
+import HomePage from "./components/HomePage"
+
+
+
+function App() {
+    return (
+        <BrowserRouter>
+          <Routes>
+           
+            <Route path="/" element={< HomePage/>}/>
+            
+          
+
+          </Routes>
+        </BrowserRouter>
+      );
+}
+
+export default App;
--- server/server.js
+++ server/server.js
@@ -1,32 +1,32 @@
-// const express = require('express');
-// const dotenv = require("dotenv").config();
-// const bodyParser = require('body-parser');
-// const  cors = require('cors');
-// const connectDb = require("./config/dbConnection");
+const express = require('express');
+const dotenv = require("dotenv").config();
+const bodyParser = require('body-parser');
+const  cors = require('cors');
+const connectDb = require("./config/dbConnection");
 
 
-// connectDb();
-
-
-
-
-
-// const app = express();
-
-
-// const port = process.env.PORT || 3002;
+connectDb();
 
 
 
-// // Middleware
-// app.use(bodyParser.json({extended: true }));
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(cors());
 
 
-// const apiRoute = require('./routes/API');
+const app = express();
 
-// app.use('/api',apiRoute)
+
+const port = process.env.PORT || 3002;
+
+
+
+// Middleware
+app.use(bodyParser.json({extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+
+const apiRoute = require('./routes/API');
+
+app.use('/api',apiRoute)
 
 
 
